86396ee91718cba1c015fd61a54ee54a67ae55e3
Steven Bowman <stevenbowmanjr@gmail.com>
2019-09-17 11:39:27 -0700

    added array of objects with q&a and choices

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,1 +1,39 @@
-$("#tester").text("insert this text as a test")
+$(document).ready(function () {
+
+let questions = [
+    {
+        q: "Hemmingway's Old Man and the Sea is set in what nation?",
+        a: "Cuba",
+        choices: ["USA", "Greenland", "Cuba", "Spain"]
+    }
+    {
+        q: "Dante's 8th circle of Hell had thieves, liars,  magicians and who?",
+        a: "Politicians",
+        choices: ["Politicians", "Murderers", "Tax-collectors", "Teachers"]
+    }
+    {
+        q: "How many shillings did Sherlock Holmes pay for what His Stradivarius violin?",
+        a: "55",
+        choices: ["120", "55", "70", "90"]
+    }
+    {
+        q: "Which great Russia Author first achieved literary acclaim in his twenties with his semi-autobiographical trilogy, Childhood, Boyhood, and Youth (1852–1856)"
+        a: "Tolstoy"
+        choices: ["dostoevsky", "Pushkin", "Tolstoy", "Solzhenitsyn"]
+    }
+ 
+]
+
+let time = 15;
+
+
+
+
+
+
+
+
+
+
+
+}
